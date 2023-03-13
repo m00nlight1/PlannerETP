b43c2934148d2ee7f2a5758517d125cf8dcc1238
Naumova Sofya Alekseevna <isip_c.a.naumova@mpt.ru>
2023-03-13 22:10:26 +0300

    Injection dio container

--- lib/app/di/init_di.config.dart
+++ lib/app/di/init_di.config.dart
@@ -7,6 +7,7 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
+import 'package:planner_etp/app/data/dio_container.dart' as _i5;
 import 'package:planner_etp/app/data/main_app_config.dart' as _i4;
 import 'package:planner_etp/app/domain/app_config.dart' as _i3;
 
@@ -39,6 +40,7 @@ extension GetItInjectableX on _i1.GetIt {
       _i4.TestAppConfig(),
       registerFor: {_test},
     );
+    gh.singleton<_i5.DioContainer>(_i5.DioContainer(gh<_i3.AppConfig>()));
     return this;
   }
 }
