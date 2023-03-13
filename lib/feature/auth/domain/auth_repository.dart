4d9e1e222d4cf82121d577b50a1de9456c743d02
Naumova Sofya Alekseevna <isip_c.a.naumova@mpt.ru>
2023-03-13 22:40:40 +0300

    Creating an AuthRepository

---
+++ lib/feature/auth/domain/auth_repository.dart
@@ -0,0 +1,26 @@
+abstract class AuthRepository {
+  Future<dynamic> signUp({
+    required String password,
+    required String email,
+    required String username
+  });
+
+  Future<dynamic> signIn({
+    required String password,
+    required String email
+  });
+
+  Future<dynamic> getProfile();
+
+  Future<dynamic> updateUser({
+    String? email,
+    String? username
+  });
+
+  Future<dynamic> updatePassword({
+    required String oldPassword,
+    required String newPassword
+  });
+
+  Future<dynamic> refreshToken({required String refreshToken});
+}
