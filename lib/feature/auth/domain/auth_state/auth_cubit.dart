07967eb568c2223207c12ca8fe1843b3ce010ae5
Naumova Sofya Alekseevna <isip_c.a.naumova@mpt.ru>
2023-03-13 23:06:16 +0300

    Global AuthCubit for authorization management

---
+++ lib/feature/auth/domain/auth_state/auth_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:planner_etp/feature/auth/domain/entities/user_entity/user_entity.dart';
+
+part 'auth_state.dart';
+
+part 'auth_cubit.freezed.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthState.notAuthorized());
+}
