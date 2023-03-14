7c18156159a753352e4317b4b1bf44aaad79b855
Naumova Sofya Alekseevna <isip_c.a.naumova@mpt.ru>
2023-03-14 11:18:37 +0300

    Initializing AuthCubit, adding GlobalProvides

--- lib/app/presentation/main_app_builder.dart
+++ lib/app/presentation/main_app_builder.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:planner_etp/app/di/init_di.dart';
 import 'package:planner_etp/app/domain/app_builder.dart';
 import 'package:planner_etp/app/presentation/config/router/app_router.dart';
 import 'package:planner_etp/app/presentation/config/theme/app_theme.dart';
+import 'package:planner_etp/feature/auth/domain/auth_repository.dart';
+import 'package:planner_etp/feature/auth/domain/auth_state/auth_cubit.dart';
 
 class MainAppBuilder implements AppBuilder {
   @override
   Widget buildApp() {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme,
-      routes: routes,
+    return _GlobalProviders(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: lightTheme,
+        routes: routes,
+      ),
     );
+  }
+}
+
+class _GlobalProviders extends StatelessWidget {
+  const _GlobalProviders({Key? key, required this.child}) : super(key: key);
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [
+      BlocProvider(create: (context) => AuthCubit(locator<AuthRepository>()))
+    ], child: child);
   }
 }
