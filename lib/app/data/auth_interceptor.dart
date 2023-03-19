68eea8bb4ee914b219dd3e49fcfda0996ae48990
Naumova Sofya Alekseevna <isip_c.a.naumova@mpt.ru>
2023-03-19 14:01:37 +0300

    Implementation of obtaining a profile and updating a token

--- lib/app/data/auth_interceptor.dart
+++ lib/app/data/auth_interceptor.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:planner_etp/app/data/dio_container.dart';
 import 'package:planner_etp/app/di/init_di.dart';
 import 'package:planner_etp/feature/auth/domain/auth_state/auth_cubit.dart';
 
@@ -15,6 +16,24 @@ class AuthInterceptor extends QueuedInterceptor {
       final headers = options.headers;
       headers["Authorization"] = "Bearer $accessToken";
       super.onRequest(options.copyWith(headers: headers), handler);
+    }
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    if (err.response?.statusCode == 401) {
+      try {
+        await locator.get<AuthCubit>().refreshToken();
+        final request = await locator
+            .get<DioContainer>()
+            .dio
+            .request(err.requestOptions.path);
+        return handler.resolve(request);
+      } catch (_) {
+        super.onError(err, handler);
+      }
+    } else {
+      super.onError(err, handler);
     }
   }
 }
