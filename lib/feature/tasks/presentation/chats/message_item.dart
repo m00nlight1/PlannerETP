04ee224dec7dec02c9eea2ed409653b43f3fbfdc
Naumova Sofya Alekseevna <isip_c.a.naumova@mpt.ru>
2023-04-03 23:17:13 +0300

    Implementation of user comment display

--- lib/feature/tasks/presentation/chats/message_item.dart
+++ lib/feature/tasks/presentation/chats/message_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jiffy/jiffy.dart';
 import 'package:planner_etp/app/presentation/app_loader.dart';
 import 'package:planner_etp/feature/auth/domain/auth_state/auth_cubit.dart';
 import 'package:planner_etp/feature/tasks/domain/chat/message_entity.dart';
@@ -28,21 +29,143 @@ class _MessageItemState extends State<MessageItem> {
         if (userEntity?.userState?.connectionState == ConnectionState.waiting) {
           return const AppLoader();
         }
-        return Align(
-          alignment: messageEntity.user?.id == userEntity?.id
-              ? Alignment.centerRight
-              : Alignment.centerLeft,
-          child: Card(
-            elevation: 8,
-            color: Colors.white,
-            child: Column(children: [
-              Text(messageEntity.content),
-              Text(messageEntity.user?.email ?? ""),
-            ]),
-          ),
-        );
+        if (messageEntity.user?.id == userEntity?.id) {
+          return _MessageOwnTile(message: messageEntity);
+        } else {
+          return _MessageTile(message: messageEntity);
+        }
+        // return Align(
+        //   alignment: messageEntity.user?.id == userEntity?.id
+        //       ? Alignment.centerRight
+        //       : Alignment.centerLeft,
+        //   child: Card(
+        //     elevation: 8,
+        //     color: Colors.white,
+        //     child: Column(children: [
+        //       Text(messageEntity.content),
+        //       Text(messageEntity.user?.email ?? ""),
+        //     ]),
+        //   ),
+        // );
       },
       listener: (context, state) {},
+    );
+  }
+}
+
+class _MessageTile extends StatelessWidget {
+  const _MessageTile({
+    Key? key,
+    required this.message,
+  }) : super(key: key);
+
+  final MessageEntity message;
+
+  static const _borderRadius = 26.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(_borderRadius),
+                  topRight: Radius.circular(_borderRadius),
+                  bottomRight: Radius.circular(_borderRadius),
+                ),
+              ),
+              child: Padding(
+                padding:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("${message.user?.username}"),
+                    const SizedBox(height: 7),
+                    Text(message.content, style: TextStyle(color: Colors.black),),
+                  ],
+                ),
+                // child: Text(message.content ?? ''),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                Jiffy(message.sentTo.toLocal()).jm,
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 11,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MessageOwnTile extends StatelessWidget {
+  const _MessageOwnTile({
+    Key? key,
+    required this.message,
+  }) : super(key: key);
+
+  final MessageEntity message;
+
+  static const _borderRadius = 26.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFF0d74ba),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(_borderRadius),
+                  bottomRight: Radius.circular(_borderRadius),
+                  bottomLeft: Radius.circular(_borderRadius),
+                ),
+              ),
+              child: Padding(
+                padding:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 20),
+                child: Text(message.content,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                Jiffy(message.sentTo.toLocal()).jm,
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 11,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/feature/tasks/presentation/chats/messages_list.dart
+++ lib/feature/tasks/presentation/chats/messages_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jiffy/jiffy.dart';
 import 'package:planner_etp/app/presentation/app_loader.dart';
 import 'package:planner_etp/feature/tasks/domain/state/detail/detail_task_cubit.dart';
 import 'package:planner_etp/feature/tasks/presentation/chats/message_item.dart';
@@ -16,13 +18,13 @@ class MessagesList extends StatelessWidget {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8),
             child: ListView.separated(
-              shrinkWrap: true,
               reverse: true,
               itemCount: state.messageList.length + 1,
               separatorBuilder: (context, index) {
                 if (index == state.messageList.length - 1) {
                   return SizedBox(
-                    child: Text(state.messageList[index].sentTo.toString()),
+                    child: _DateLable(dateTime: state.messageList[index].sentTo)
+                    // Text(state.messageList[index].sentTo.toString()),
                   );
                 }
                 if (state.messageList.length == 1) {
@@ -32,10 +34,9 @@ class MessagesList extends StatelessWidget {
                 } else if (index <= state.messageList.length) {
                   final message = state.messageList[index];
                   final nextMessage = state.messageList[index + 1];
-                  if ((message.sentTo.toLocal().toString() ==
-                      nextMessage.sentTo.toLocal().toString())) {
-                    return SizedBox(
-                      child: Text(message.sentTo.day.toString()),
+                  if (!Jiffy(message.sentTo.toLocal()).isSame(nextMessage.sentTo.toLocal(), Units.DAY)) {
+                    return _DateLable(
+                      dateTime: message.sentTo
                     );
                   } else {
                     return const SizedBox.shrink();
@@ -61,6 +62,75 @@ class MessagesList extends StatelessWidget {
           child: Text("Сообщений нет"),
         );
       },
+    );
+  }
+}
+
+class _DateLable extends StatefulWidget {
+  const _DateLable({
+    Key? key,
+    required this.dateTime,
+  }) : super(key: key);
+
+  final DateTime dateTime;
+
+  @override
+  __DateLableState createState() => __DateLableState();
+}
+
+class __DateLableState extends State<_DateLable> {
+  late String dayInfo;
+
+  @override
+  void initState() {
+    final createdAt = Jiffy(widget.dateTime);
+    final now = DateTime.now();
+
+    if (Jiffy(createdAt).isSame(now, Units.DAY)) {
+      dayInfo = 'Сегодня';
+    } else if (Jiffy(createdAt)
+        .isSame(now.subtract(const Duration(days: 1)), Units.DAY)) {
+      dayInfo = 'Вчера';
+    } else if (Jiffy(createdAt).isAfter(
+      now.subtract(const Duration(days: 7)),
+      Units.DAY,
+    )) {
+      dayInfo = createdAt.EEEE;
+    } else if (Jiffy(createdAt).isAfter(
+      Jiffy(now).subtract(years: 1),
+      Units.DAY,
+    )) {
+      dayInfo = createdAt.MMMd;
+    } else {
+      dayInfo = createdAt.MMMd;
+    }
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 12),
+            child: Text(
+              dayInfo,
+              style: const TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
