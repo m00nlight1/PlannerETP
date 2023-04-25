add825fcc10d8f1b855d868019c9f67bcd461d65
Naumova Sofya Alekseevna <isip_c.a.naumova@mpt.ru>
2023-04-25 17:55:56 +0300

    Creating a category selection screen

---
+++ lib/feature/tasks/presentation/tasks/category_task/add_category_task_screen.dart
@@ -0,0 +1,456 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
+import 'package:planner_etp/app/presentation/components/app_text_field.dart';
+import 'package:planner_etp/feature/tasks/domain/image_storage_service.dart';
+import 'package:planner_etp/feature/tasks/domain/state/task_cubit.dart';
+
+class AddCategoryTaskScreen extends StatefulWidget {
+  const AddCategoryTaskScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _AddCategoryTaskScreenState();
+}
+
+class _AddCategoryTaskScreenState extends State<AddCategoryTaskScreen> {
+  DateTime selectedDate = DateTime.now();
+  TimeOfDay selectedTime = TimeOfDay.now();
+  DateTime startWorkDateTime = DateTime.now();
+  DateTime endWorkDateTime = DateTime.now();
+  bool showDate = false;
+  bool showTime = false;
+  bool showStartWorkDateTime = false;
+  bool showEndWorkDateTime = false;
+
+  final titleController = TextEditingController();
+  final companyController = TextEditingController();
+  final masterController = TextEditingController();
+  final representativeController = TextEditingController();
+  final equipmentLevelController = TextEditingController();
+  final staffLevelController = TextEditingController();
+  final resultsOfTheWorkController = TextEditingController();
+  final commentsController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  final Storage storage = Storage();
+
+  File? imageFile;
+  String? fileName;
+
+  void _getImgFromGallery() async {
+    PickedFile? pickedFile = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+      maxWidth: 320,
+      maxHeight: 320,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+  // Select for Date
+  Future<DateTime> _selectDate(BuildContext context) async {
+    final selected = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2025),
+    );
+    if (selected != null && selected != selectedDate) {
+      setState(() {
+        selectedDate = selected;
+      });
+    }
+    return selectedDate;
+  }
+
+  // Select for Time
+  Future<TimeOfDay> _selectTime(BuildContext context) async {
+    final selected = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (selected != null && selected != selectedTime) {
+      setState(() {
+        selectedTime = selected;
+      });
+    }
+    return selectedTime;
+  }
+
+  Future _selectStartWorkDateTime(BuildContext context) async {
+    final date = await _selectDate(context);
+    if (date == null) return;
+    final time = await _selectTime(context);
+
+    if (time == null) return;
+    setState(() {
+      startWorkDateTime = DateTime(
+        date.year,
+        date.month,
+        date.day,
+        time.hour,
+        time.minute,
+      );
+    });
+  }
+
+  Future _selectEndWorkDateTime(BuildContext context) async {
+    final date = await _selectDate(context);
+    if (date == null) return;
+    final time = await _selectTime(context);
+
+    if (time == null) return;
+    setState(() {
+      endWorkDateTime = DateTime(
+        date.year,
+        date.month,
+        date.day,
+        time.hour,
+        time.minute,
+      );
+    });
+  }
+
+  String getStartWorkDateTime() =>
+      DateFormat('yyyy-MM-dd kk:mm').format(startWorkDateTime);
+
+  String getEndWorkDateTime() =>
+      DateFormat('yyyy-MM-dd – kk:mm').format(endWorkDateTime);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Создать задачу"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              if (imageFile != null) {
+                fileName = storage.getRandomString(7);
+                storage.uploadImage(imageFile!.path, fileName!);
+              }
+              context.read<TaskCubit>().createTask({
+                "title": titleController.text,
+                "content": commentsController.text,
+                "startOfWork": startWorkDateTime.toString(),
+                "endOfWork": endWorkDateTime.toString(),
+                "imageUrl": fileName,
+                "contractorCompany": companyController.text,
+                "responsibleMaster": masterController.text,
+                "representative": representativeController.text,
+                "equipmentLevel": equipmentLevelController.text,
+                "staffLevel": staffLevelController.text,
+                "resultsOfTheWork": resultsOfTheWorkController.text,
+                "idCategory": 1
+              });
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.done),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Center(
+            child: Column(
+              children: [
+                //title
+                AppTextField(
+                    hintText: 'Название',
+                    obscureText: false,
+                    prefixIcon: const Icon(Icons.rate_review_outlined,
+                        color: Color(0xFF0d74ba)),
+                    controller: titleController,
+                    validator: (title) =>
+                        title != null ? 'Введите название' : null),
+                const SizedBox(height: 10),
+                //start and end datetime
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //start of work
+                    Card(
+                      color: Colors.grey.shade200,
+                      child: SizedBox(
+                        width: 166,
+                        height: 135,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Начало работ',
+                                  style: theme.textTheme.headlineSmall),
+                              const SizedBox(height: 10),
+                              Row(
+                                children: [
+                                  showStartWorkDateTime
+                                      ? const Icon(Icons.date_range_outlined,
+                                          color: Color(0xFF0d74ba))
+                                      : const SizedBox(),
+                                  const SizedBox(width: 5),
+                                  showStartWorkDateTime
+                                      ? Flexible(
+                                          child: Text(
+                                          getStartWorkDateTime(),
+                                          style: theme.textTheme.bodyMedium,
+                                        ))
+                                      : const SizedBox(),
+                                ],
+                              ),
+                              MaterialButton(
+                                onPressed: () {
+                                  _selectStartWorkDateTime(context);
+                                  showStartWorkDateTime = true;
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 5.0),
+                                  child: Text(
+                                    'Установить\nдату и время',
+                                    style: theme.textTheme.labelMedium,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    //end of work
+                    Card(
+                      color: Colors.grey.shade200,
+                      child: SizedBox(
+                        width: 166,
+                        height: 135,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Окончание работ',
+                                  style: theme.textTheme.headlineSmall),
+                              const SizedBox(height: 10),
+                              Row(
+                                children: [
+                                  showEndWorkDateTime
+                                      ? const Icon(Icons.date_range_outlined,
+                                          color: Color(0xFF0d74ba))
+                                      : const SizedBox(),
+                                  const SizedBox(width: 5),
+                                  showEndWorkDateTime
+                                      ? Flexible(
+                                          child: Text(
+                                          getEndWorkDateTime(),
+                                          style: theme.textTheme.bodyMedium,
+                                        ))
+                                      : const SizedBox(),
+                                ],
+                              ),
+                              MaterialButton(
+                                onPressed: () {
+                                  _selectEndWorkDateTime(context);
+                                  showEndWorkDateTime = true;
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 5.0),
+                                  child: Text(
+                                    'Установить\nдату и время',
+                                    style: theme.textTheme.labelMedium,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                //contractor company
+                AppTextField(
+                    hintText: 'Компания исполнитель',
+                    obscureText: false,
+                    prefixIcon: const Icon(Icons.rate_review_outlined,
+                        color: Color(0xFF0d74ba)),
+                    controller: companyController,
+                    validator: (company) => company != null
+                        ? 'Укажите компанию исполнителя'
+                        : null),
+                const SizedBox(height: 10),
+                //responsible master
+                AppTextField(
+                    hintText: 'Ответственный мастер',
+                    obscureText: false,
+                    prefixIcon: const Icon(Icons.rate_review_outlined,
+                        color: Color(0xFF0d74ba)),
+                    controller: masterController,
+                    validator: (master) => master != null
+                        ? 'Укажите ответственного мастера'
+                        : null),
+                const SizedBox(height: 10),
+                //representative
+                AppTextField(
+                    hintText: 'Представитель',
+                    obscureText: false,
+                    prefixIcon: const Icon(Icons.rate_review_outlined,
+                        color: Color(0xFF0d74ba)),
+                    controller: representativeController,
+                    validator: (representative) => representative != null
+                        ? 'Укажите представителя'
+                        : null),
+                const SizedBox(height: 10),
+                //images
+                Card(
+                  color: Colors.grey.shade200,
+                  child: imageFile == null
+                      ? SizedBox(
+                          width: 342,
+                          height: 100,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Фото',
+                                    style: theme.textTheme.headlineSmall),
+                                MaterialButton(
+                                  onPressed: () {
+                                    _getImgFromGallery();
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 55.0),
+                                    child: Text(
+                                      'Добавить медиафайл',
+                                      style: theme.textTheme.labelMedium,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      : SizedBox(
+                          width: 342,
+                          height: 320,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Фото',
+                                    style: theme.textTheme.headlineSmall),
+                                const SizedBox(height: 10),
+                                Image.file(
+                                  imageFile!,
+                                  fit: BoxFit.cover,
+                                ),
+                                const SizedBox(height: 10),
+                                MaterialButton(
+                                  onPressed: () {
+                                    _getImgFromGallery();
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 75.0),
+                                    child: Text(
+                                      'Выбрать другой медиафайл',
+                                      style: theme.textTheme.labelMedium,
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                ),
+                const SizedBox(height: 10),
+                //equipment level
+                AppTextField(
+                    hintText: 'Уровень оснащения',
+                    obscureText: false,
+                    prefixIcon: const Icon(Icons.rate_review_outlined,
+                        color: Color(0xFF0d74ba)),
+                    controller: equipmentLevelController,
+                    validator: (equipmentLevel) => equipmentLevel != null
+                        ? 'Укажите уровень оснащения'
+                        : null),
+                const SizedBox(height: 10),
+                //staff level
+                AppTextField(
+                    hintText: 'Уровень песонала',
+                    obscureText: false,
+                    prefixIcon: const Icon(Icons.rate_review_outlined,
+                        color: Color(0xFF0d74ba)),
+                    controller: staffLevelController,
+                    validator: (staffLevel) => staffLevel != null
+                        ? 'Укажите уровень персонала'
+                        : null),
+                const SizedBox(height: 10),
+                //resultsOfTheWork
+                Card(
+                  color: Colors.grey.shade200,
+                  child: SizedBox(
+                    width: 342,
+                    height: 100,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Результаты работы',
+                              style: theme.textTheme.headlineSmall),
+                          Expanded(
+                            child: TextFormField(
+                              controller: resultsOfTheWorkController,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                //comments
+                Card(
+                  color: Colors.grey.shade200,
+                  child: SizedBox(
+                    width: 342,
+                    height: 100,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Комментарии',
+                              style: theme.textTheme.headlineSmall),
+                          Expanded(
+                            child: TextFormField(
+                              controller: commentsController,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
